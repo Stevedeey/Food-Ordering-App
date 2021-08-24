7f51d13e481978da9456303e19aad6991adf3b60
firsthus <festus.audu@decagon.dev>
2021-08-24 21:06:15 +0100

    initial commit

---
+++ src/main/java/com/byteworks/byteworksfoodapp/ByteworksFoodAppApplication.java
@@ -0,0 +1,13 @@
+package com.byteworks.byteworksfoodapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteworksFoodAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByteworksFoodAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/byteworks/byteworksfoodapp/ByteworksFoodAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byteworks.byteworksfoodapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteworksFoodAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
